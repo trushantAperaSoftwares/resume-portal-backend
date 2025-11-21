96dfb47b8738bf2ecbfaf839ab19c0b6205c8f02
trushantAperaSoftwares <trushant@aperasoftwares.com>
2025-11-21 17:37:23 +0530

    Update users.controller.ts
    update guards

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -23,25 +23,10 @@ import { UsersUpdateDto } from './dto/users-update.dto';
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
-
-<<<<<<< HEAD
-// create 
-  @Post()
-  async createUser(@Body() createDto: UsersCreateDto) {
-    return this.usersService.createUser(createDto);
-  }
   
-  // 
-  @UseGuards(AuthGuard, RolesGuard)
-  @SetMetadata('role', [Role.ADMIN])
-=======
-  // @UseGuards(AuthGuard)
-  // @SetMetadata('role', [Role.ADMIN, Role.HR])
-  // @UseGuards(RolesGuard)
   @UseGuards(AuthGuard)
   @SetMetadata('roles', [Role.ADMIN])
   @UseGuards(RolesGuard)
->>>>>>> 4e2483f (guards update)
   @Post('create')
   async create(@Body() createDto: UsersCreateDto) {
     return this.usersService.createUser(createDto);
@@ -54,16 +39,10 @@ export class UsersController {
   }
 
   // get all data
-<<<<<<< HEAD
-  @Get()
-  @UseGuards(AuthGuard, RolesGuard)
-  @SetMetadata('role', [Role.ADMIN])
-=======
   @UseGuards(AuthGuard)
   @SetMetadata('roles', [Role.ADMIN])
   @UseGuards(RolesGuard)
   @Get()
->>>>>>> 4e2483f (guards update)
   async getAllUsers(
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
